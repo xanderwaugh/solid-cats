82c0f823ebd3cf3166de3eb80102a2d01668dba7
Xander Waugh <xanderwaugh531@gmail.com>
2023-04-29 16:50:41 -0400

    hydrate csr

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,5 @@
-import { StartClient, mount } from "solid-start/entry-client";
-mount(() => <StartClient />, document);
-// import { hydrate } from "solid-js/web";
-// hydrate(() => <StartClient />, document);
+import { StartClient } from "solid-start/entry-client";
+// import { mount } from "solid-start/entry-client";
+// mount(() => <StartClient />, document);
+import { hydrate } from "solid-js/web";
+hydrate(() => <StartClient />, document);
